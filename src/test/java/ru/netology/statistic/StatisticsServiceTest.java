374f1ca95fae43d464c33cc510fa9b6da8b751e8
eva.gribova <eva.gribova.93@mail.ru>
2024-02-22 13:12:41 +0400

    Tests added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxOne() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 3};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxTwo() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
